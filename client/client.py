f552ea845bd33a7fa5805aa63d89818513a48a43
HarmanSmith <leonel.diaz.16842@gmail.com>
2022-01-24 14:54:12 -0300

    first decorators

--- client/client.py
+++ client/client.py
@@ -5,11 +5,13 @@ providing functions that will be used from tests.
 """
 import requests
 from requests import Response
+from hamcrest import is_, is_not
 
 
 class ReqResClient:
     def __init__(self, url: str):
         self.url = url
+
 
     # Here we define all the API endpoints.
     # Endpoints are private as the consumer classes won't need to access urls directly.
@@ -17,7 +19,42 @@ class ReqResClient:
     def __users_url(self) -> str:
         return f"{self.url}/users"
 
+
     # Here we define all operations our API does, as python functions
     def get_users(self) -> Response:
         """Calls endpoint GET /users"""
         return requests.get(self.__users_url)
+
+    @property
+    def id(self):
+        return requests.get(self.__users_url).json()['data']['id']
+
+    @property
+    def email(self):
+        return requests.get(self.__users_url).json()['data']['email']
+
+    @property
+    def first_name(self):
+        return requests.get(self.__users_url).json()['data']['first_name']
+
+    @property
+    def last_name(self):
+        return requests.get(self.__users_url).json()['data']['last_name']
+
+    @property
+    def avatar(self):
+        return requests.get(self.__users_url).json()['data']['avatar']
+
+
+# For each user retrieved verify URL inside the avatar property is a non empty string and a valid url
+
+# When we call POST /api/users with a valid payload we should get a 201 response.
+# Response should be well formed Json object that should contain the following fields:
+# name, job, id, createdAt
+
+# When we call POST /api/login using a payload with name but no password,
+# we should get a 400 response. error should read "Missing password"
+
+# When we call POST /api/login using a valid payload, we should get 200 response.
+# The response object should contain the "token" property and its value should be a
+# non-null and non-empty string
--- tests/test_using_client.py
+++ tests/test_using_client.py
@@ -9,3 +9,21 @@ def test_users_return_200_and_not_empty_client(reqres_client: ReqResClient):
     response = reqres_client.get_users()
     assert_that(response.status_code, is_(200))
     assert_that(response.json(), is_not(None))
+
+
+# For each user retrieved verify URL inside the avatar property is a non empty string and a valid url
+def test_users_return_200_and_fields(reqres_client: ReqResClient):
+    response = reqres_client.get_users()
+    assert_that(response.status_code, is_(200))
+    assert_that(reqres_client.avatar, is_not(None))
+
+# When we call POST /api/users with a valid payload we should get a 201 response.
+# Response should be well formed Json object that should contain the following fields:
+# name, job, id, createdAt
+
+# When we call POST /api/login using a payload with name but no password,
+# we should get a 400 response. error should read "Missing password"
+
+# When we call POST /api/login using a valid payload, we should get 200 response.
+# The response object should contain the "token" property and its value should be a
+# non-null and non-empty string
